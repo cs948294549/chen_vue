aeecdc85accefac24447ac1d14a5a9497129c8ea
chensong1 <chensong1@corp.netease.com>
2025-04-14 16:51:23 +0800

    aa

--- src/main.js
+++ src/main.js
@@ -16,24 +16,35 @@ const protocol = window.location.protocol === 'https:' ? 'https:' : 'http:';
 
 console.log("用户使用协议类型===", protocol, window.location.protocol)
 
+// if(protocol=="http:"){
+//   Vue.use(new VueSocketIO({
+//    debug: true,
+//    connection: SocketIO("http://chen.test.com/sock", {
+//     autoConnect: false ,// 自动连接
+//    }),
+//    options: { path: '/sock/socket.io' },
+//    extraHeaders: { "Access-Control-Allow-Origin": "*" }
+//   }));
+// }else{
+//   Vue.use(new VueSocketIO({
+//    debug: false,
+//    connection: 'https://chen.test.com/',
+//    options: { path: '/sock/socket.io' },
+//    extraHeaders: { "Access-Control-Allow-Origin": "*" }
+//   }));
+// }
+
 if(protocol=="http:"){
   Vue.use(new VueSocketIO({
    debug: true,
-   connection: SocketIO("http://chen.test.com/sock/", {
-    autoConnect: false ,// 自动连接
-    }),
-   options: { path: '/socket.io' },
-   extraHeaders: { "Access-Control-Allow-Origin": "*" }
+   connection: SocketIO('http://chen.test.com', {path:'/sock/socket.io'}),
   }));
 }else{
   Vue.use(new VueSocketIO({
-   debug: false,
-   connection: 'https://chen.test.com/sock/',
-   options: { path: '/sock/socket.io' },
-   extraHeaders: { "Access-Control-Allow-Origin": "*" }
+   debug: true,
+   connection: SocketIO('https://chen.test.com', {path:'/sock/socket.io'}),
   }));
 }
-
 
 
 const app = new Vue({
